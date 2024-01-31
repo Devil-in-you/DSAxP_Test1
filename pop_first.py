ccd1678a904ddb7f7f5f7abbc65fbc7ef62a7eea
Devil-in-you <prkashved2@gmail.com>
2024-01-31 19:06:23 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   pop_first.py

---
+++ pop_first.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next_node = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next_node): # is not None: can aslo be used
+            pre = temp
+            temp = temp.next_node
+        self.tail = pre
+        self.tail.next_node = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+            self.head = None
+        return temp
+    
