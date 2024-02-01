13ae9f18a0694d339e28117b0632dda5b766adf4
Devil-in-you <prkashved2@gmail.com>
2024-02-02 00:23:37 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   doubly_linked_list/pop.py

--- doubly_linked_list/pop.py
+++ doubly_linked_list/pop.py
@@ -28,3 +28,16 @@ class DoublyLinkedList:
             self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        temp.prev = None
+        self.length -= 1
+        if self.length == 0:
+            self.head == None
+            self.tail == None
+        return temp
