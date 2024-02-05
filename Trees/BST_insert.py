2fdc6b32728fea6a32f25725c10ae79ce84c88bc
Devil-in-you <prkashved2@gmail.com>
2024-02-05 23:34:15 +0530

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	modified:   Trees/BST_insert.py

--- Trees/BST_insert.py
+++ Trees/BST_insert.py
@@ -7,3 +7,10 @@ class Node:
 class BinerySearchTree:
     def __init__(self):
         self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root == None:
+            self.root = new_node
+        temp = new_node
+        
