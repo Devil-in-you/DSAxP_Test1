0b8852f656316816887e695f296ce568d8717661
Devil-in-you <prkashved2@gmail.com>
2024-02-11 18:57:01 +0530

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	modified:   Recursive binery search tree/rBST_contains.py

---
+++ Recursive binery search tree/rBST_contains.py
@@ -0,0 +1,12 @@
+def __r_contains(self, current_node, value):
+    if current_node == None:
+        return False
+    if current_node == value:
+        return True
+    if current_node > value:
+        return self.__r_contains(current_node.left, value)
+    if current_node < value:
+        return self.__r_contains(current_node.right, value)
+
+def r_contains(self, value):
+    return self.__r_contains(self.root, value) 
